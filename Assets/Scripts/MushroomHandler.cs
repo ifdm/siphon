0e45a78a836eb8452f1c83e3867506bd4223598e
Ezra <rathmaster@gmail.com>
2013-11-25 10:21:14 -0700

    Amended previous commit

---
+++ Assets/Scripts/MushroomHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MushroomHandler : MonoBehaviour {
+
+	float minHeightReached = 0f;
+
+	void MushroomBounceEvent(){
+		print ("Mushroom Event: " + minHeightReached);	
+	}
+
+
+	void FixedUpdate(){
+		if (transform.position.y < minHeightReached)
+						minHeightReached = transform.position.y;
+	}
+}
