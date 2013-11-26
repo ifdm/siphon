f074ed0a945a902c3641b27ded098bd574fa0812
Trey Cordova <treycordova@hotmail.com>
2013-11-25 22:22:09 -0700

    Added in frameOne information and meta;

--- Assets/Scripts/MushroomBounce.cs
+++ Assets/Scripts/MushroomBounce.cs
@@ -3,6 +3,6 @@ using System.Collections;
 
 public class MushroomBounce : MonoBehaviour {
 	void OnCollisionEnter2D (Collision2D col) {
-		Console.log ("Hit mushroom");
+		//Console.log ("Hit mushroom");
 	}
 }
