19973e6dcaec070d773d480bdef1cc636b225df4
Trey Cordova <treycordova@hotmail.com>
2013-11-30 15:12:42 -0700

    Built out a seed throwing implementation;

---
+++ Assets/Scripts/PlayerThrow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerThrow : MonoBehaviour {
+
+	public GameObject seed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			// Throw seed
+			Instantiate(this.seed, transform.position, Quaternion.identity);
+		}
+	}
+}
---
+++ Assets/Scripts/SeedThrow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeedThrow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		rigidbody2D.AddForce(new Vector2(1000, 30)); 
+	}
+	
+	void OnCollisionEnter2D (Collision2D col) {
+		Debug.Log (col.gameObject.tag);
+		if(col.gameObject.tag == "Ground") {
+			Debug.Log ("Collided");
+			DestroyObject(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
