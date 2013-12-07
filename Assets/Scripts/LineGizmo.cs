1be055d935441f4cd7cb0974e368292e0c860c2f
tie372 <tie.372@gmail.com>
2013-12-07 11:42:22 -0700

    Add LineGizmo; Improve ground prefabs;

---
+++ Assets/Scripts/LineGizmo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineGizmo : MonoBehaviour {
+	void OnDrawGizmos () {
+		Gizmos.color = Color.cyan;
+		var collider = (EdgeCollider2D)collider2D;
+		for(var i = 0; i < collider.pointCount - 1; i++) {
+			Gizmos.DrawLine (transform.position + (Vector3)collider.points[i], transform.position + (Vector3)collider.points[i + 1]);
+		}
+	}
+}
