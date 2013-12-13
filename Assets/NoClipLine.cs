b4be212ebf5051f61afaf561492e95ffacce6746
Trey Cordova <treycordova@hotmail.com>
2013-12-13 16:53:02 -0700

    Working on one-way collision for player and ground.

---
+++ Assets/NoClipLine.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoClipLine : MonoBehaviour {
+
+	public GameObject line;
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.tag == "Player") {
+			if(col.gameObject.rigidbody2D.velocity.y > 0) {
+				col.gameObject.layer = LayerMask.NameToLayer("Ghost Player");
+			}
+		}
+	}
+	
+	void OnCollisionExit2D(Collision2D col) {
+		if(col.gameObject.tag == "Player") {
+			col.gameObject.layer = LayerMask.NameToLayer("Player");
+		}
+	}
+}
