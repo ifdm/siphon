e2ce5156e86eb3e2a437c585487d0ea377792e91
Trey Cordova <treycordova@hotmail.com>
2013-12-14 15:35:42 -0700

    Changed edge collider to box collider for trigger;

--- Assets/NoClipLine.cs
+++ Assets/NoClipLine.cs
@@ -4,17 +4,17 @@ using System.Collections;
 public class NoClipLine : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D col) {
-		Debug.Log (col.gameObject.tag);
 		if(col.gameObject.tag == "Player") {
-			if(col.gameObject.rigidbody2D.velocity.y > 0) {
+			//if(col.gameObject.rigidbody2D.velocity.y > 0) {
+				Debug.Log ("Ghost");
 				col.gameObject.layer = LayerMask.NameToLayer("Ghost Player");
-			}
+			//}
 		}
 	}
 	
 	void OnTriggerExit2D(Collider2D col) {
-		Debug.Log (col.gameObject.tag);
 		if(col.gameObject.tag == "Player") {
+			Debug.Log ("Player");
 			col.gameObject.layer = LayerMask.NameToLayer("Player");
 		}
 	}
