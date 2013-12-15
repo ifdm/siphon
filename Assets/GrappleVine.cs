c91fb284ba3fc7f88608267f5ebafb54d52fe473
tie372 <tie.372@gmail.com>
2013-12-14 18:07:12 -0700

    Start GrappleVine; Player StateMachine; LedgeGrab;

---
+++ Assets/GrappleVine.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrappleVine : MonoBehaviour {
+
+	private Vector3 endPoint;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 up = transform.position;
+		up.y += 1000;
+		RaycastHit2D cast = Physics2D.Linecast(transform.position, up, 1 << LayerMask.NameToLayer("Ground"));
+		if(cast) {
+			endPoint = cast.point;
+		}
+
+		Debug.Log(endPoint);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawLine(transform.position, endPoint, Color.red);
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,24 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
 	[HideInInspector]
-	public bool facingRight = true;   // For determining which way the player is currently facing.
+	public bool facingRight = true;
 	[HideInInspector]
-	public bool jump = false;    // Condition for whether the player should jump.
+	public bool jump = false;
 	
-	
-	public float moveForce = 190f;   // Amount of force added to move the player left and right.
-	public float maxSpeed = 5f;    // The fastest the player can travel in the x axis.
-	public float jumpForce = 1000f;   // Amount of force added when the player jumps.
+	public float moveForce = 190f;
+	public float maxSpeed = 5f;
+	public float jumpForce = 1000f;
 
-	private Transform groundCheck;   // A position marking where to check if the player is grounded.
-	private bool grounded = false;   // Whether or not the player is grounded.
+	private Transform feet;
 
+	enum PlayerState {WALKING, JUMPING, LEDGING, CLIMBING};
+	private PlayerState state;
 
 	bool bounced = false;
 	float bounce1 = 875f;
 	float bounce2 = 975f;
 	
-	void MushroomBounceEvent(){
+	void MushroomBounceEvent() {
 		if (bounced) {
 			rigidbody2D.AddForce (new Vector2 (0, bounce2));
 		}
@@ -31,72 +31,105 @@ public class PlayerMovement : MonoBehaviour
 		}
 	}
 
-	
-	void Awake()
-	{
-		// Setting up references.
-		groundCheck = transform.Find("Player Feet");
+	void Awake() {
+		state = PlayerState.WALKING;
+		feet = transform.Find("Player Feet");
 	}
 	
-	
-	void Update()
-	{
-		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
-		
-		// If the jump button is pressed and the player is grounded then the player should jump.
-		if(Input.GetButtonDown("Jump") && grounded)
-			jump = true;
+	void Update() {
+		switch(state) {
+			case PlayerState.WALKING:
+				rigidbody2D.isKinematic = false;
+				bounced = false;
+				
+				if(!Physics2D.Linecast(transform.position, feet.position, 1 << LayerMask.NameToLayer("Ground"))) {
+					state = PlayerState.JUMPING;
+				}
+			break;
 
-		if (grounded)
-						bounced = false;
-	}
-	
-	
-	void FixedUpdate ()
-	{
-		// Cache the horizontal input.
-		float h = Input.GetAxis("Horizontal");
-		
-		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-		if(h * rigidbody2D.velocity.x < maxSpeed)
-			// ... add a force to the player.
-			rigidbody2D.AddForce(Vector2.right * h * moveForce);
-		
-		// If the player's horizontal velocity is greater than the maxSpeed...
-		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
-			// ... set the player's velocity to the maxSpeed in the x axis.
-			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
-		
-		// If the input is moving the player right and the player is facing left...
-		if(h > 0 && !facingRight)
-			// ... flip the player.
-			Flip();
-		// Otherwise if the input is moving the player left and the player is facing right...
-		else if(h < 0 && facingRight)
-			// ... flip the player.
-			Flip();
-		
-		// If the player should jump...
-		if(jump)
-		{	
-			// Add a vertical force to the player.
-			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+			case PlayerState.JUMPING:
+				rigidbody2D.isKinematic = false;
 			
-			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
-			jump = false;
+				if(Physics2D.Linecast(transform.position, feet.position, 1 << LayerMask.NameToLayer("Ground"))) {
+					state = PlayerState.WALKING;
+				}
+				
+				else if(rigidbody2D.velocity.y < 0) {
+					Vector3 eye1 = transform.position;
+					Vector3 eye2 = transform.position;
+					eye1.y += 0.5f;
+					eye2.y += 0.5f;
+					if(facingRight){eye2.x += 0.7f;}
+					else{eye2.x -= 0.7f;}
+					
+					Debug.DrawLine(eye1, eye2, Color.blue);
+					
+					if(Physics2D.Linecast(eye1, eye2, 1 << LayerMask.NameToLayer("Ground"))) {
+						state = PlayerState.LEDGING;
+					}
+				}
+			break;
+
+			case PlayerState.LEDGING:
+				rigidbody2D.isKinematic = true;
+			break;
+			
+			case PlayerState.CLIMBING:
+				rigidbody2D.isKinematic = true;
+			break;
+		}
+	}	
+	
+	void FixedUpdate() {
+		switch(state) {
+			case PlayerState.WALKING:
+				Move();
+				Jump();
+			break;
+			
+			case PlayerState.JUMPING:
+				Move();
+			break;
+			
+			case PlayerState.LEDGING:
+				Jump();
+			break;
+			
+			case PlayerState.CLIMBING:
+				Climb();
+				Jump();
+			break;
 		}
 	}
 	
-	
-	void Flip ()
-	{
-		// Switch the way the player is labelled as facing.
-		facingRight = !facingRight;
+	void Move() {
+		float h = Input.GetAxis("Horizontal");
+				
+		if(h * rigidbody2D.velocity.x < maxSpeed) {
+			rigidbody2D.AddForce(Vector2.right * h * moveForce);
+		}
 		
-		// Multiply the player's x local scale by -1.
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
+		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed) {
+			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+		}
+		
+		if((h > 0 && !facingRight) || (h < 0 && facingRight)) {
+			facingRight = !facingRight;
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+		}
+	}
+	
+	void Climb() {
+		//
+	}
+	
+	void Jump() {
+		if(Input.GetButtonDown("Jump")) {
+			rigidbody2D.isKinematic = false;
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+			state = PlayerState.JUMPING;
+		}
 	}
 }
