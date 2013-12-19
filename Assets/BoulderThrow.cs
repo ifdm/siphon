ac185bb24262e1c1689534a22fae17d46e7aa732
Trey Cordova <treycordova@hotmail.com>
2013-12-18 20:21:12 -0700

    Work on boulder;

--- Assets/BoulderThrow.cs
+++ Assets/BoulderThrow.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class BoulderThrow : MonoBehaviour {
 
+	private float timer;
+
+	public float boulderRate = 7;
+	public GameObject boulder;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		timer += Time.deltaTime;
+		if(timer >= boulderRate) {
+			timer = 0;
+			var instance = (GameObject)Instantiate(boulder);
+			instance.transform.position = transform.position;
+			instance.rigidbody2D.AddForce(new Vector2(-10, 20));
+		}
 	}
 }
--- Assets/TouchOfDeath.cs
+++ Assets/TouchOfDeath.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class TouchOfDeath : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D col) {
 		if(col.gameObject.tag == "Player") {
-			Application.LoadLevel("levelOne");
+			Application.LoadLevel(Application.loadedLevel);
 		}
 	}
 }
