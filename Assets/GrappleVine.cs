2e72b719ad4b7dd203ace1f5466b808f1b58c96b
tie372 <tie.372@gmail.com>
2013-12-18 20:11:25 -0800

    GrappleVine doesn't touch ground;

--- Assets/GrappleVine.cs
+++ Assets/GrappleVine.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class GrappleVine : MonoBehaviour {
 
+	private Vector3 startPoint;
 	private Vector3 endPoint;
 	private bool dirty;
 
 	// Use this for initialization
 	void Start () {
+		startPoint = new Vector3(transform.position.x, transform.position.y + 3, transform.position.z);
 		Vector3 up = transform.position;
 		up.y += 1000;
 		RaycastHit2D cast = Physics2D.Linecast(transform.position, up, 1 << LayerMask.NameToLayer("Ground"));
@@ -17,8 +19,8 @@ public class GrappleVine : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.DrawLine(transform.position, endPoint, Color.red);
-		RaycastHit2D cast = Physics2D.Linecast(transform.position, endPoint, 1 << LayerMask.NameToLayer("Player"));
+		Debug.DrawLine(startPoint, endPoint, Color.red);
+		RaycastHit2D cast = Physics2D.Linecast(startPoint, endPoint, 1 << LayerMask.NameToLayer("Player"));
 		if(cast) {
 			PlayerMovement player = cast.rigidbody.gameObject.GetComponent("PlayerMovement") as PlayerMovement;
 			if(!dirty) {
