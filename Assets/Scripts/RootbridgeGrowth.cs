047bad93d35d52624b0ddc808ac0a78bedf855ee
Trey Cordova <treycordova@hotmail.com>
2013-12-19 21:33:45 -0700

    Rootbridge implementation (Rough);
    Moved scripts into Script Folder;

---
+++ Assets/Scripts/RootbridgeGrowth.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RootbridgeGrowth : MonoBehaviour {
+
+	void Start() {
+		if(!safe()) {
+			DestroyObject(gameObject);
+		}
+	}
+
+	bool safe() {
+		Transform body = transform.Find("body");
+		Vector3 extents = body.gameObject.GetComponent<BoxCollider2D>().size * .5f;
+
+		Vector3 eye1 = transform.position;
+		Vector3 eye2 = transform.position;
+		extents.x *= transform.lossyScale.x;
+
+		eye1.x += extents.x + .01f;
+		eye2.x += extents.x + .02f;
+
+		if(Physics2D.Linecast(eye1, eye2, 1 << LayerMask.NameToLayer("Ground"))) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
