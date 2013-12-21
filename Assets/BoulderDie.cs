0a2607cf6a0238c91a06e858a8619b6196bccd3a
tie372 <tie.372@gmail.com>
2013-12-21 15:19:14 -0700

    No more boulder epidemic;

---
+++ Assets/BoulderDie.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoulderDie : MonoBehaviour {
+
+	private float health = 10f;
+		
+	void Update () {
+		health -= Time.deltaTime;
+		if(health <= 0){Destroy(gameObject);}
+	}
+}
