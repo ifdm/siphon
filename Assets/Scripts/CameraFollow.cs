4071cf87a6770bfdaddad7f6911876c940246654
tie372 <tie.372@gmail.com>
2013-12-21 15:41:11 -0700

    New mouselook model;

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,11 +5,19 @@ public class CameraFollow : MonoBehaviour {
 	
 	public float smooth = 16;
 	public GameObject player;
+	public float mouseThreshold = 5;
 
 	void Update() {
-		Vector3 target = Vector3.Lerp(camera.ScreenToWorldPoint(Input.mousePosition), player.transform.position, .75f);
-		target.z = transform.position.z;
+		Vector3 mouse = camera.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 p = player.transform.position;
+		mouse.z = 0;
+		p.z = 0;
 		
+		Vector3 target;
+		if(Mathf.Abs(mouse.x - p.x) > mouseThreshold || Mathf.Abs(mouse.y - p.y) > mouseThreshold){target = Vector3.Lerp(mouse, p, .5f);}
+		else{target = p;}
+		
+		target.z = transform.position.z;
 		transform.position = Vector3.Lerp(transform.position, target, Mathf.Clamp(smooth * Time.deltaTime, 0.0f, 1.0f));
 	}
 }
