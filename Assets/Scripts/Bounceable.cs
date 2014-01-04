9c5a0101e4e94acda708ebb7e16c3c4eca451c88
Trey Cordova <treycordova@hotmail.com>
2014-01-03 23:01:17 -0700

    Mushroom converted to a trigger;
    Allows player to walk through the mushroom;
    Built Bounceable script so other object can bounce and double bounce;

---
+++ Assets/Scripts/Bounceable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounceable : MonoBehaviour {
+
+	[HideInInspector] public bool bounced = false;
+
+	void Bounce() {
+		bounced = true;
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.tag == "Ground") {
+			bounced = false;
+		}
+	}
+}
