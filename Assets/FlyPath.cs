a2ef23393c91f33580fbd9b9b816085e0c5cecd3
Trey Cordova <treycordova@hotmail.com>
2014-01-04 12:05:20 -0700

    Preliminary, unprefab'ed fly trap;

---
+++ Assets/FlyPath.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyPath : MonoBehaviour {
+
+	public float velocity = 2;
+	private bool entered = false;
+
+	void Start () {
+
+	}
+	
+	void FixedUpdate () {
+		rigidbody2D.velocity = new Vector2(velocity, rigidbody2D.velocity.y);
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.gameObject.tag == "Portal") {
+			Debug.Log("Teleporting.");
+
+			Porter porter = col.gameObject.GetComponent<Porter>();
+			porter.Port(gameObject);
+		}
+		else if(col.gameObject.name == "Player") {
+			// Ignore.
+		}
+		else if(!entered) {
+			Debug.Log("Turning around.");
+			velocity = -velocity;
+			entered = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+		entered = false;
+	}
+}
---
+++ Assets/Porter.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Porter : MonoBehaviour {
+
+	public GameObject next;
+	public GameObject previous;
+	public float delay = 1f;
+	// Next is 1, previous is 0
+	[HideInInspector] public int direction = 1;
+	private Porter nextScript;
+	private Porter previousScript;
+	private bool destination = false;
+	private float timer = 0;
+	private float gracePeriod = 1f;
+
+	void Start() {
+		nextScript = (next) ? next.GetComponent<Porter>() : null;
+		previousScript = (previous) ? previous.GetComponent<Porter>() : null;
+	}
+	
+	void Update() {
+		if((Time.time - timer) > (gracePeriod + delay)) {
+			destination = false;
+			timer = Time.time;
+			Debug.Log(destination);
+		}
+	}
+
+	public void Port(GameObject other) {
+		if(!destination) {
+			if(direction == 1) {
+				if(next) {
+					other.transform.position = next.transform.position;
+					nextScript.destination = true;
+					direction = nextScript.direction = 0;
+				}
+			}
+			else if(direction == 0) {
+				if(previous) {
+					other.transform.position = previous.transform.position;
+					previousScript.destination = true;
+					direction = previousScript.direction = 1;
+				}
+			}
+		}
+	}
+}
