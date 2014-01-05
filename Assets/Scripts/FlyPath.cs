fd60d3d2c04dc7167dd9d45ebd6c57f0eb529be4
Trey Cordova <treycordova@hotmail.com>
2014-01-04 21:21:09 -0700

    Fixed null destiny by throwing when slots > 0;

---
+++ Assets/Scripts/FlyPath.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyPath : MonoBehaviour {
+
+	public Vector2 velocity = new Vector2(-2, 0);
+	private bool porting = false;
+	private bool entered = false;
+	
+	void FixedUpdate() {
+		rigidbody2D.velocity = velocity;
+	}
+}
--- Assets/Scripts/Porter.cs
+++ Assets/Scripts/Porter.cs
@@ -11,8 +11,8 @@ public class Porter : MonoBehaviour {
 	public directions direction;
 
 	// Next is true, previous is false
-	[HideInInspector] public bool forward = true;
-	[HideInInspector] public bool ready = true;
+	[HideInInspector] public bool forward;
+	[HideInInspector] public bool porting;
 
 	private Porter nextScript;
 	private Porter previousScript;
@@ -20,18 +20,14 @@ public class Porter : MonoBehaviour {
 	private GameObject item;
 
 	void Start() {
-		ready = true;
+		forward = true;
+		porting = false;
+
 		nextScript = (next) ? next.GetComponent<Porter>() : null;
 		previousScript = (previous) ? previous.GetComponent<Porter>() : null;
 	}
-	
-	void Update() {
-		if(ready && item) {
-			Send(item);
-		}
-	}
 
-	private void Send(GameObject other) {
+	public void Port(GameObject other) {
 		GameObject portal = (forward) ? next : previous;
 		Porter script = (forward) ? nextScript : previousScript;
 		FlyPath path = other.GetComponent<FlyPath>();
@@ -60,44 +56,21 @@ public class Porter : MonoBehaviour {
 					y = 0;
 					break;
 			}
+			// Set velocity
 			path.velocity = new Vector2(x, y);
-			// Resolve hidden rendering
-			other.renderer.enabled = true;
-			// Tell the next portal, "You are not prepared."
-			script.ready = false;
 			// Decide next portal
 			forward = script.forward = (forward) ? false : true;
+			// 
 		}
 		else {
 			Debug.Log("No portal attached. Not porting.");
 		}
 	}
 
-	public void Port(GameObject other) {
-		if(ready) {
-			Debug.Log("Sending");
-			Send(other);
-		}
-		else {
-			Debug.Log("Not sending");
-			FlyPath path = other.GetComponent<FlyPath>();
-			// Stop the object
-			path.velocity = new Vector2(0, 0);
-			// Hide the object
-			//other.renderer.enabled = false;
-			// Keep a copy
-			item = other;
-		}
-	}
-
 	void OnTriggerEnter2D(Collider2D col) {
-		Debug.Log(col.gameObject.name);
-		Debug.Log(ready);
-	}
-
-	void OnTriggerExit2D(Collider2D col) {
-		if(col.gameObject.name == "Fly") {
-			ready = true;
+		if(col.gameObject.GetComponent<FlyPath>()) {
+			col.gameObject.rigidbody2D.isKinematic = true;
+			Port(col.gameObject);
 		}
 	}
 }
