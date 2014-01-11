1019841600e953de74b88a88fce00ccaffb94737
tie372 <tie.372@gmail.com>
2014-01-11 14:51:59 -0700

    Add gear; SpinToWin script;
    Small mushroom and seed adjustments.

---
+++ Assets/Scripts/lib/SpinToWin.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinToWin : MonoBehaviour {
+	public float speed = 1f;
+	
+	void Update () {
+		transform.Rotate(new Vector3(0, 0, speed * Time.deltaTime));
+	}
+}
