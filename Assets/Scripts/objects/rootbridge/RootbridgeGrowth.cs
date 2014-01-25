7ceb18558cbb20b924f4f6cd278189986afa3f5a
Trey Cordova <treycordova@hotmail.com>
2014-01-25 13:21:00 -0700

    Rootbridge now has a growth grace period;

--- Assets/Scripts/objects/rootbridge/RootbridgeGrowth.cs
+++ Assets/Scripts/objects/rootbridge/RootbridgeGrowth.cs
@@ -3,51 +3,37 @@ using System.Collections;
 
 public class RootbridgeGrowth : MonoBehaviour {
 
-	private GameObject player;
-	private Vector3 l1;
-	private Vector3 l2;
+	private GameObject seed;
+	private Transform body;
 
 	void Start() {
-		player = GameObject.Find("Player");
+		seed = GameObject.Find("Seed");
+		body = transform.Find("body");
+
 		if(!safe()) {
 			DestroyObject(gameObject);
 		}
 	}
 
-	void Update() {
-		Debug.DrawLine(l1, l2, Color.red);
-	}
-
 	bool safe() {
-		GameObject seed = GameObject.Find("Seed");
 		float radius = seed.GetComponent<CircleCollider2D>().radius * seed.transform.lossyScale.x;
-		Transform body = transform.Find("body");
+
 		Vector3 extents = body.gameObject.GetComponent<BoxCollider2D>().size * .5f;
+		extents = Vector2.Scale(extents, transform.lossyScale);
 
 		Vector3 p1 = transform.position;
 		Vector3 p2 = transform.position;
-		extents = Vector2.Scale(extents, transform.lossyScale);
-
-		p1.y -= extents.y + 0.01f;
-		p2.y = p1.y - 0.6f - radius;
-
-		if(Physics2D.Linecast(p1, p2, 1 << LayerMask.NameToLayer("Ground"))) {
-			return false;
-		}
 
 		p1.x += 0.1f;
-		p1.y = transform.position.y + extents.y + 0.01f;
+		p1.y += extents.y + 0.01f;
 
 		p2.x += radius + 0.1f;
-		p2.y = transform.position.y + extents.y + 0.01f;
-
-		l1 = p1;
-		l2 = p2;
+		p2.y += extents.y + 0.01f;
 
 		// Right
 		if(Physics2D.Linecast(p1, p2, 1 << LayerMask.NameToLayer("Ground"))) {
 			transform.position = new Vector3(transform.position.x - extents.x + radius, transform.position.y, transform.position.z);
-			return true;
+			return !isGrounded(transform.position);
 		}
 
 		p1.x -= 0.2f + radius;
@@ -56,9 +42,46 @@ public class RootbridgeGrowth : MonoBehaviour {
 		// Left
 		if(Physics2D.Linecast(p1, p2, 1 << LayerMask.NameToLayer("Ground"))) {
 			transform.position = new Vector3(transform.position.x + extents.x - radius, transform.position.y, transform.position.z);
+			return !isGrounded(transform.position);
+		}
+
+		return false;
+	}
+
+	bool isGrounded(Vector3 position) {
+		float padding = 0.1f;
+		float radius = seed.GetComponent<CircleCollider2D>().radius * seed.transform.lossyScale.x;
+		Vector3 extents = body.gameObject.GetComponent<BoxCollider2D>().size;
+
+		Vector3 p1 = position;
+		Vector3 p2 = position;
+
+		p1.x -= (extents.x + radius * 2) - padding;
+		p1.y -= 0.2f;
+		p2.x += (extents.x + radius * 2) - padding;
+		p2.y -= 0.2f;
+
+		// First Ground Check
+		if(Physics2D.Linecast(p1, p2, 1 << LayerMask.NameToLayer("Ground"))) {
 			return true;
 		}
 
-		return true;
+		p1.y -= 0.2f;
+		p2.y -= 0.2f;
+
+		// Second Ground Check
+		if(Physics2D.Linecast(p1, p2, 1 << LayerMask.NameToLayer("Ground"))) {
+			return true;
+		}
+
+		p1.y -= 0.2f;
+		p2.y -= 0.2f;
+
+		// Third Ground Check
+		if(Physics2D.Linecast(p1, p2, 1 << LayerMask.NameToLayer("Ground"))) {
+			return true;
+		}
+
+		return false;
 	}
 }
