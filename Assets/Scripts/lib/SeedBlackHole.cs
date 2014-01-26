4009631f8b2481b6a5f5a0429b91f23988bab51f
tie372 <tie.372@gmail.com>
2014-01-25 18:11:48 -0800

    Seed Black Hole;

---
+++ Assets/Scripts/lib/SeedBlackHole.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeedBlackHole : MonoBehaviour {
+	
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.tag == "Seed") {
+			Destroy(col.gameObject);
+		}
+	}
+}
