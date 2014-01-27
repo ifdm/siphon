115b6f1d235cbce5d31e4a52c134f11854659825
Trey Cordova <treycordova@hotmail.com>
2014-01-26 21:48:58 -0700

    Trying to hone player animations;

--- Assets/Scripts/objects/player/states/FallingState.cs
+++ Assets/Scripts/objects/player/states/FallingState.cs
@@ -14,11 +14,11 @@ public class FallingState : PlayerState {
 		if(player.canLedgeGrab() && ledgeGrace == 0) {
 			player.ChangeState(PlayerState.Ledging);
 		}
-		else if(player.isIdle()) {
-			player.ChangeState(PlayerState.Idling);
-		}
 		else if(player.isGrounded()) {
 			player.ChangeState(PlayerState.Running);
+		}
+		else if(player.isIdle()) {
+			player.ChangeState(PlayerState.Idling);
 		}
 		else if(player.canClimb()) {
 			player.ChangeState(PlayerState.Climbing);
@@ -33,7 +33,6 @@ public class FallingState : PlayerState {
 	}
 
 	public override void Exit(PlayerControl player, PlayerState to) {
-		player.animator.TimeScale = 2f;
 		player.animator.Set("Land");
 		if(fallVelocity < -player.physics.lethalVelocity && player.isGrounded()){Application.LoadLevel(Application.loadedLevel);}
 	}
