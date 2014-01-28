f71dcb9cc355ea4fc30a0c22c52d769899c67f2d
Trey Cordova <treycordova@hotmail.com>
2014-01-27 22:44:48 -0700

    Added top raycasts to rootbridge;
    Player can no longer plant the rootbridge in tight corners;

--- Assets/Scripts/objects/rootbridge/RootbridgeGrowth.cs
+++ Assets/Scripts/objects/rootbridge/RootbridgeGrowth.cs
@@ -50,33 +50,58 @@ public class RootbridgeGrowth : MonoBehaviour {
 
 	bool isGrounded(Vector3 position, float radius) {
 		float padding = 0.5f;
+		float spacer = 0.3f;
 		Vector3 extents = body.gameObject.GetComponent<BoxCollider2D>().size;
 
 		Vector3 p1 = position;
 		Vector3 p2 = position;
 
 		p1.x -= (extents.x + radius * 2) - padding;
-		p1.y -= 0.2f;
+		p1.y -= spacer;
 		p2.x += (extents.x + radius * 2) - padding;
-		p2.y -= 0.2f;
+		p2.y -= spacer;
 
-		// First Ground Check
+		// First Bottom Ground Check
 		if(Physics2D.Linecast(p1, p2, 1 << LayerMask.NameToLayer("Ground"))) {
 			return true;
 		}
 
-		p1.y -= 0.2f;
-		p2.y -= 0.2f;
+		p1.y -= spacer;
+		p2.y -= spacer;
 
-		// Second Ground Check
+		// Second Bottom Ground Check
 		if(Physics2D.Linecast(p1, p2, 1 << LayerMask.NameToLayer("Ground"))) {
 			return true;
 		}
 
-		p1.y -= 0.2f;
-		p2.y -= 0.2f;
+		p1.y -= spacer;
+		p2.y -= spacer;
 
-		// Third Ground Check
+		// Third Bottom Ground Check
+		if(Physics2D.Linecast(p1, p2, 1 << LayerMask.NameToLayer("Ground"))) {
+			return true;
+		}
+
+		p1.y += spacer * 4;
+		p2.y += spacer * 4;
+
+		// First Top Ground Check
+		if(Physics2D.Linecast(p1, p2, 1 << LayerMask.NameToLayer("Ground"))) {
+			return true;
+		}
+
+		p1.y += spacer;
+		p2.y += spacer;
+
+		// Second Top Ground Check
+		if(Physics2D.Linecast(p1, p2, 1 << LayerMask.NameToLayer("Ground"))) {
+			return true;
+		}
+
+		p1.y += spacer;
+		p2.y += spacer;
+
+		// Third Top Ground Check
 		if(Physics2D.Linecast(p1, p2, 1 << LayerMask.NameToLayer("Ground"))) {
 			return true;
 		}
