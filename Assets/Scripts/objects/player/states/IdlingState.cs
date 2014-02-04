86985583936a475aa474451c3a9430b4e80e8313
tie372 <tie.372@gmail.com>
2014-02-03 23:47:46 -0700

    Re-add slope alignment;
    I am a horrible person.

--- Assets/Scripts/objects/player/states/IdlingState.cs
+++ Assets/Scripts/objects/player/states/IdlingState.cs
@@ -12,6 +12,15 @@ public class IdlingState : PlayerState {
 			player.ChangeState(PlayerState.Jumping);
 		}
 	}
+	
+	public override void Update(PlayerControl player) {
+		Vector2 normal = player.normal();
+		if(normal != Vector2.zero) {
+			Transform animation = player.transform.Find("Animation");
+			if(player.transform.lossyScale.x < 0){normal.x *= -1;}
+			animation.rotation = Quaternion.Lerp(animation.rotation, Quaternion.FromToRotation(Vector3.up, (Vector3) normal), 4 * Time.deltaTime);
+		}
+	}
 
 	public override void Enter(PlayerControl player, PlayerState from) {
 		player.animator.Add("Idle", true);
