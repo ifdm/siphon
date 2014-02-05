3e3789c2075884213d5086f48bbd955d28456cd2
tie372 <tie.372@gmail.com>
2014-02-04 17:58:44 -0700

    Fix camera stuff in preparation for perspective;

--- Assets/Scripts/objects/camera/CameraEase.cs
+++ Assets/Scripts/objects/camera/CameraEase.cs
@@ -8,28 +8,31 @@ public class CameraEase : MonoBehaviour {
 
 	private float x, y, w, h;
 	private Transform player;
-	private float zVel = 0;
+
+	[HideInInspector] public static bool dirty;
 
 	public void Start() {
+		size -= 5; // Legacy support ew
+
 		BoxCollider2D box = GetComponent<BoxCollider2D>();
 		w = box.size.x * transform.lossyScale.x;
 		h = box.size.y * transform.lossyScale.y;
 		x = transform.position.x - box.size.x * transform.lossyScale.x / 2;
 		y = transform.position.y - box.size.y * transform.lossyScale.y / 2;
-		
+
 		player = GameObject.Find("Player").transform;
+		y -= .5f;
 	}
 
 	public void Update() {
+		CameraFollow camera = GameObject.Find("Main Camera").GetComponent<CameraFollow>();
 		if(player.position.x > x && player.position.y > y && player.position.x < x + w && player.position.y < y + h) {
-			Transform camera = GameObject.Find("Main Camera").transform;
-			float z = Mathf.SmoothDamp(camera.position.z, -10 - size, ref zVel, smooth);
-			camera.position = new Vector3(camera.position.x, camera.position.y, z);
+			camera.additionalZ = size;
+			camera.zSmooth = smooth;
+			CameraEase.dirty = true;
 		}
-		else {
-			Transform camera = GameObject.Find("Main Camera").transform;
-			float z = Mathf.SmoothDamp(camera.position.z, -10, ref zVel, smooth);
-			camera.position = new Vector3(camera.position.x, camera.position.y, z);
+		else if(!CameraEase.dirty) {
+			camera.additionalZ = 0;
 		}
 	}
 }
--- Assets/Scripts/objects/camera/CameraFollow.cs
+++ Assets/Scripts/objects/camera/CameraFollow.cs
@@ -5,60 +5,37 @@ public class CameraFollow : MonoBehaviour {
 	
 	public float smooth = 0.5f;
 	[HideInInspector] public GameObject player;
-	[HideInInspector] public float targetSize;
-	[HideInInspector] public float defaultSize;
-	[HideInInspector] public float sizeSmooth;
-	[HideInInspector] public float defaultSizeSmooth;
+	[HideInInspector] public float additionalZ = 0;
+	[HideInInspector] public float zSmooth = .5f;
 
-	public float padding = 1;
-
-	private float sizeVel = 0;
+	private float z;
+	private float zVel = 0;
 	private Vector3 posVel = Vector3.zero;
 
 	void Start() {
 		player = GameObject.Find("Player");
-		float z = transform.position.z;
-		transform.position = player.transform.position;
-		transform.position = new Vector3(transform.position.x, transform.position.y, z);
-		defaultSize = camera.orthographicSize;
-		targetSize = defaultSize;
-		sizeSmooth = 0.65f;
-		defaultSizeSmooth = sizeSmooth;
+		z = transform.position.z;
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y, z);
+
 		camera.transparencySortMode = TransparencySortMode.Orthographic;
 	}
 
 	void OnPreRender() {
-		Vector3 my = transform.position;
-		Vector3 mouse = camera.ScreenToWorldPoint(Input.mousePosition);
-		Vector2 p2d = (Vector2)player.transform.position;
-		Vector2 scale = (Vector2)player.transform.lossyScale;
-		BoxCollider2D box = player.GetComponent<BoxCollider2D>();
-		p2d += Vector2.Scale(box.center, scale);
-		Vector3 p = new Vector3(p2d.x, p2d.y, my.z);
-		mouse.z = my.z;
-		
-		float h = camera.orthographicSize - padding;
-		float w = (camera.orthographicSize * camera.aspect) - padding;
-
-		Vector3 target = p;
 		float s = smooth;
-
-		if(p.x - target.x > w){target.x = p.x - w;}
-		else if(target.x - p.x > w){target.x = p.x + w;}
-		else if(p.y - target.y > h){target.y = p.y - h;}
-		else if(target.y - p.y > h){target.y = p.y + h;}
-
 		if(Mathf.Abs(player.rigidbody2D.velocity.y) > 20) {
-			float z = 0.3f;
 			s = smooth * (1 - Mathf.Clamp((Mathf.Abs(player.rigidbody2D.velocity.y) - 20) / 20, 0.0f, 0.75f));
-			camera.orthographicSize = Mathf.SmoothDamp(camera.orthographicSize, 10, ref sizeVel, z);
-			sizeSmooth = 0.65f;
+			z = Mathf.SmoothDamp(z, -20 - additionalZ, ref zVel, 0.3f);
+			zSmooth = smooth;
 		}
 		else {
-			float z = sizeSmooth;
-			camera.orthographicSize = Mathf.SmoothDamp(camera.orthographicSize, targetSize, ref sizeVel, z);
+			z = Mathf.SmoothDamp(z, -10 - additionalZ, ref zVel, zSmooth);
 		}
 
+		Vector2 p2d = (Vector2)player.transform.position + Vector2.Scale(player.GetComponent<BoxCollider2D>().center, (Vector2)player.transform.lossyScale);
+		Vector3 target = new Vector3(p2d.x, p2d.y, z);
+
 		transform.position = Vector3.SmoothDamp(transform.position, target, ref posVel, s);
+
+		CameraEase.dirty = false;
 	}
 }
