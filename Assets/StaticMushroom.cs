4c8869d53cda53e08653dce562c510de947fa5ec
tie372 <tie.372@gmail.com>
2014-02-09 14:14:19 -0700

    StaticMushroom; Plant folder rubix cubing;

---
+++ Assets/StaticMushroom.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticMushroom : MonoBehaviour {
+	
+	public float delay = 1;
+
+	public void Start() {
+		transform.Find("Mushroom").gameObject.GetComponent<MushroomAnimator>().TimeScale = 0;
+	}
+
+	public void OnTriggerEnter2D(Collider2D col) {
+		if(col.tag == "Player") {
+			StartCoroutine(delayGrowth());
+		}
+	}
+	
+	IEnumerator delayGrowth() {
+		yield return new WaitForSeconds(delay);
+		transform.Find("Mushroom").gameObject.GetComponent<MushroomAnimator>().TimeScale = 1;
+	}
+}
