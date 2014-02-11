9b8b23cee38a1a5b4d4494bb7b9071fe46d29b27
tie372 <tie.372@gmail.com>
2014-02-11 00:30:11 -0700

    Add Static Rootbridge prefab;

---
+++ Assets/StaticRootBridge.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticRootBridge : MonoBehaviour {
+	
+	public float delay = 1;
+
+	public void Start() {
+		transform.Find("Rootbridge").Find("Animation").gameObject.GetComponent<Animator>().speed = 0;
+	}
+
+	public void OnTriggerEnter2D(Collider2D col) {
+		if(col.tag == "Player") {
+			StartCoroutine(delayGrowth());
+		}
+	}
+	
+	IEnumerator delayGrowth() {
+		yield return new WaitForSeconds(delay);
+		transform.Find("Rootbridge").Find("Animation").gameObject.GetComponent<Animator>().speed = 1;
+	}
+}
