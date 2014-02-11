355da1db2cc94c2c12fcf07d332409f44f792df3
tie372 <tie.372@gmail.com>
2014-02-11 15:30:49 -0700

    Clean PlayerThrow;

--- Assets/Scripts/objects/player/PlayerThrow.cs
+++ Assets/Scripts/objects/player/PlayerThrow.cs
@@ -46,54 +46,6 @@ public class PlayerThrow : MonoBehaviour {
 	}
 	
 	void Update () {
-		PlayerControl player = gameObject.GetComponent<PlayerControl>();
-		PlayerPhysics playerPhysics = gameObject.GetComponent<PlayerPhysics>();
-
-		/*if(CanThrow(player)) {
-			Vector3 playerPos = transform.position;
-			BoxCollider2D box = GetComponent<BoxCollider2D>();
-			playerPos.x += box.center.x * transform.lossyScale.x;
-			playerPos.y += box.center.y * transform.lossyScale.y;
-
-			GameObject thrownSeed = (GameObject)Instantiate(this.seed, playerPos, Quaternion.identity);
-			thrownSeed.name = "Seed";
-
-			Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-			Plane xy = new Plane(Vector3.forward, new Vector3(0, 0, 0));
-			float distance;
-			xy.Raycast(ray, out distance);
-			Vector2 p = ray.GetPoint(distance);
-			Vector2 v = (p - new Vector2(transform.position.x, transform.position.y));
-
-			float angle = (float)Mathf.Atan2 (v.x, v.y);
-			angle = angle * Mathf.Rad2Deg;
-
-			//Adjust player rotation
-			if(playerPhysics.facingRight && angle < 0 || !playerPhysics.facingRight && angle > 0)
-				playerPhysics.ChangeDirection();
-
-			float top = 0;
-			float bottom = 180;
-			float tDangle = Mathf.Abs(Mathf.DeltaAngle (angle, top));
-			float bDangle = Mathf.Abs(Mathf.DeltaAngle (angle, bottom));
-
-			//If outside of the bounds above and below, simply throw straight down
-			if(tDangle < topDeadZone || bDangle < bottomDeadZone)
-			{
-				v = new Vector2(0, 0);
-			}
-
-			thrownSeed.GetComponent<SeedThrow>().destiny = slots[activeSlot];
-			while(slotQueues[activeSlot].Count > 0) {
-				Destroy((GameObject)slotQueues[activeSlot].Dequeue());
-			}
-
-			thrownSeed.GetComponent<SeedThrow>().destiny = slots[activeSlot];
-			thrownSeed.GetComponent<SeedThrow>().queue = slotQueues[activeSlot];
-			thrownSeed.rigidbody2D.AddForce(v.normalized * throwForce);
-			player.animator.Set("Throw", false, 1);
-		}*/
-		
 		if(slots.Length > 0) {
 			bool targeting = Input.GetAxis("Seed Horizontal") != 0 || Input.GetAxis("Seed Vertical") != 0 || Input.GetMouseButton(0);
 			bool throwing = Input.GetAxis("Seed Throw") != 0 || Input.GetMouseButtonUp(0);
@@ -127,27 +79,15 @@ public class PlayerThrow : MonoBehaviour {
 			}
 			
 			
-			if(throwing) {
-				if(target != Vector3.zero) {
-					/*GameObject thrownSeed = (GameObject)Instantiate(this.seed, target, Quaternion.identity);
-					thrownSeed.name = "Seed";
-
-					thrownSeed.GetComponent<SeedThrow>().destiny = slots[activeSlot];
-					while(slotQueues[activeSlot].Count > 0) {
-						Destroy((GameObject)slotQueues[activeSlot].Dequeue());
-					}
-
-					thrownSeed.GetComponent<SeedThrow>().destiny = slots[activeSlot];
-					thrownSeed.GetComponent<SeedThrow>().queue = slotQueues[activeSlot];
-					player.animator.Set("Throw", false, 1);*/
-					
-					while(slotQueues[activeSlot].Count > 0) {
-						Destroy((GameObject)slotQueues[activeSlot].Dequeue());
-					}
-					slotQueues[activeSlot].Enqueue(Instantiate(slots[activeSlot], target, Quaternion.identity));
-					
-					target = Vector3.zero;
+			if(throwing && target != Vector3.zero) {
+				while(slotQueues[activeSlot].Count > 0) {
+					Plant plant = (Plant)slotQueues[activeSlot].Dequeue();
+					Destroy(plant.gameObject);
 				}
+				slotQueues[activeSlot].Enqueue(Instantiate(slots[activeSlot], target, Quaternion.identity));
+				GetComponent<PlayerControl>().animator.Set("Throw", false, 1);
+				
+				target = Vector3.zero;
 			}
 			
 			selectSeed();
@@ -172,10 +112,5 @@ public class PlayerThrow : MonoBehaviour {
 			if(activeSlot < 0){activeSlot = slots.Length - 1;}
 			//Cursor.SetCursor(cursors[activeSlot], Vector2.zero, CursorMode.Auto);
 		}
-	}
-
-	//Checks if the player can throw
-	private bool CanThrow(PlayerControl player){
-		return false && Input.GetMouseButtonDown(0) && player.state != PlayerState.Ledging && throwable && !GameObject.Find ("Seed") && slots.Length > 0;
 	}
 }
