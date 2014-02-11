45bd0e8c874bd21551b17b4aa5ac5b8bcc6fbaca
tie372 <tie.372@gmail.com>
2014-02-11 03:13:49 -0700

    Static GrappleVine; Organization;

---
+++ Assets/Scripts/objects/grapple-vine/StaticGrappleVine.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticGrappleVine : MonoBehaviour {
+	
+	public float delay = 1;
+
+	public void Start() {
+		transform.Find("GrappleVine").Find("Animation").gameObject.GetComponent<Animator>().speed = 0;
+	}
+
+	public void OnTriggerEnter2D(Collider2D col) {
+		if(col.tag == "Player") {
+			StartCoroutine(delayGrowth());
+		}
+	}
+	
+	IEnumerator delayGrowth() {
+		yield return new WaitForSeconds(delay);
+		transform.Find("GrappleVine").Find("Animation").gameObject.GetComponent<Animator>().speed = 1;
+	}
+}
