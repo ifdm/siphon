107562555d280c8905d955e7681047ef67efe7eb
Ezra <rathmaster@gmail.com>
2014-02-16 19:24:58 -0700

    Audio Triggers, Music Triggers

---
+++ Assets/Scripts/objects/musicTrigger/AudioTrigger.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+//Triggers ambient audio in the gameworld
+public class AudioTrigger : Mozart {
+	
+	private GameObject player;
+	
+	public bool repeats;
+	public float volume;
+	public string track;
+	
+	public bool allowOverlap = false;
+	public float minDelayBetweenPlayback = 0.25f;
+	
+	private float coolDown = 0.0f;
+	
+	public override void Awake() {
+		clips = new Dictionary<string, AudioClip>() {
+			{track, Resources.Load<AudioClip>(track)}
+		};
+	}
+	
+	void Start(){
+		player = GameObject.Find ("Player");
+		base.Start();
+	}
+	
+	void OnUpdate(){
+		coolDown = (coolDown <= 0.0f) ? 0.0f : coolDown - Time.deltaTime * 0.1f;
+	}
+	
+	void OnTriggerEnter2D(Collider2D col) {
+		
+		if (col.gameObject.Equals (GameObject.Find ("Player")) && coolDown <= 0.0f) 
+		{
+			if(allowOverlap)
+			{
+				Play(track, volume, repeats, 0);
+			}
+			else
+			{
+				One(track, volume, repeats, 0);
+			}
+			coolDown = minDelayBetweenPlayback;
+		}
+	}
+	
+}
+
---
+++ Assets/Scripts/objects/musicTrigger/EntityAudio.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+//A simple extension of the Mozart class allowing names of audio files to be set through the inspector.
+//Call the Play or One functions of this class when necessary inside the behavior of the relevant GameObject
+public class EntityAudio : Mozart {
+
+	public string[] names;
+
+	public override void Awake() {
+		clips = new Dictionary<string, AudioClip>();
+		foreach (string name in names){
+			clips.Add(name, Resources.Load<AudioClip>(name));
+		}
+	}
+}
