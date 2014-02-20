218344c48a6dcaa2d4be63cb932cb59a2ab8e8ec
tie372 <tie.372@gmail.com>
2014-02-20 01:19:00 -0700

    KnockOverTree scene; Fix player prefab; Comet art;

---
+++ Assets/RobotDie.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotDie : MonoBehaviour {
+
+	private bool broken = false;
+
+	public void Start() {
+		GameObject vine = GameObject.Find("GrappleVine/Animation");
+		vine.GetComponent<Animator>().speed = 0;
+	}
+
+	// Use this for initialization
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.name == "Boulder" && !broken) {
+			GetComponent<SpriteRenderer>().color = new Color(.5f, .5f, .5f);
+			GameObject vine = GameObject.Find("GrappleVine/Animation");
+			vine.GetComponent<Animator>().speed = 1;
+			broken = true;
+		}
+	}
+}
