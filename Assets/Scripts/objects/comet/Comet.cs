fa51b50450a7da17cc0463d3be9e15fec0b8bf72
Bjorn <tie.372@gmail.com>
2014-02-21 01:31:32 -0700

    RAWR;

--- Assets/Scripts/objects/comet/Comet.cs
+++ Assets/Scripts/objects/comet/Comet.cs
@@ -5,12 +5,12 @@ public class Comet : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		rigidbody2D.velocity = new Vector2(-25, -20);
+		rigidbody2D.velocity = new Vector2(-38, -30);
 	}
 
 	void OnTriggerEnter2D(Collider2D col) {
-		if(col.gameObject.name == "Death") {
-			GameObject.Find("Main Camera").GetComponent<CameraFollow>().shake = 2;
+		if(col.gameObject.name == "FallingTree") {
+			GameObject.Find("Main Camera").GetComponent<CameraFollow>().shake = .2f;
 			Destroy(gameObject);
 		}
 	}
