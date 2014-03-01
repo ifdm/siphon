1b243010a24f473944b1c930bf3adc1f3a4f9c68
tie372 <tie.372@gmail.com>
2014-02-28 17:27:16 -0700

    DelayedDestroy;

---
+++ Assets/DelayedDestroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayedDestroy : MonoBehaviour {
+
+	public float delay;
+	public GameObject victim;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.tag == "Player") {
+			StartCoroutine(destroy());
+		}
+	}
+
+	IEnumerator destroy() {
+		yield return new WaitForSeconds(delay);
+		if(victim) {
+			Destroy(victim);
+		}
+	}
+}
