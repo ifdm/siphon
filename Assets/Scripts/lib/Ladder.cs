5035c231e3a9498212baa774cc5faa885bae7aa1
tie372 <tie.372@gmail.com>
2014-03-07 16:33:25 -0700

    Climbing state is now more laddery;
    Ladder is more robust too;

--- Assets/Scripts/lib/Ladder.cs
+++ Assets/Scripts/lib/Ladder.cs
@@ -6,7 +6,7 @@ public class Ladder : MonoBehaviour {
 	void Start () {
 		Climbable climbable = GetComponent<Climbable>();
 		BoxCollider2D box = GetComponent<BoxCollider2D>();
-		climbable.startPoint = new Vector3(transform.position.x, transform.position.y - box.size.y * transform.lossyScale.y / 2, transform.position.z);
-		climbable.endPoint = new Vector3(transform.position.x, transform.position.y + box.size.y * transform.lossyScale.y / 2, transform.position.z);
+		climbable.startPoint = new Vector3(transform.position.x + (box.center.x * transform.lossyScale.x), transform.position.y + ((box.center.y - (box.size.y / 2)) * transform.lossyScale.y), transform.position.z);
+		climbable.endPoint = new Vector3(transform.position.x + (box.center.x * transform.lossyScale.x), transform.position.y + ((box.center.y + (box.size.y / 2)) * transform.lossyScale.y), transform.position.z);
 	}
 }
