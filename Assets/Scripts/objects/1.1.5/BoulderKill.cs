d8d2e431e68164ed31f430b99f72fc963f979827
Trey Cordova <treycordova@hotmail.com>
2014-03-13 23:15:31 -0600

    1.1.5 events added;

---
+++ Assets/Scripts/objects/1.1.5/BoulderKill.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoulderKill : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.gameObject.name == "RootKiller") {
+			Destroy(col.gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.name == "Root") {
+			Destroy(col.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
