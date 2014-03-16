f7ee3aeaf68767b7699047cfdb67bb9876e0557b
tie372 <tie.372@gmail.com>
2014-03-16 16:57:22 -0600

    1.1.3????;; Fixed ledging stuff;

--- Assets/Scripts/objects/1.1.3/BranchBreak.cs
+++ Assets/Scripts/objects/1.1.3/BranchBreak.cs
@@ -6,22 +6,13 @@ public class BranchBreak : MonoBehaviour {
 	// Use this for initialization
 	void OnCollisionEnter2D(Collision2D col) {
 		if(col.collider.tag == "Player") {
-			GameObject branch = GameObject.Find("Branch2");
+			GameObject branch = GameObject.Find("Branch1");
 			if(branch) {
 				HingeJoint2D joint = branch.GetComponent<HingeJoint2D>();
 				if(joint) {
 					Destroy(joint);
-					StartCoroutine(ded());
 				}
 			}
 		}
-	}
-
-	IEnumerator ded() {
-		yield return new WaitForSeconds(1.5f);
-		Destroy(GameObject.Find("Branch1"));
-		Destroy(GameObject.Find("Branch2"));
-		Destroy(GameObject.Find("Branch3"));
-		Destroy(GameObject.Find("Branch4"));
 	}
 }
