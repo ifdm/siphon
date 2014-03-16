1fbe156991b828f513d7604d8cf1d7ad3126b117
tie372 <tie.372@gmail.com>
2014-03-15 20:33:28 -0600

    CameraFrame;

---
+++ Assets/Scripts/objects/camera/CameraFrame.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFrame : MonoBehaviour {
+
+	public float size = 5;
+	public float smooth = 3.0f;
+
+	private float x, y, w, h;
+	private Transform player;
+
+	void Start () {
+		BoxCollider2D box = GetComponent<BoxCollider2D>();
+		w = box.size.x * transform.lossyScale.x;
+		h = box.size.y * transform.lossyScale.y;
+		x = transform.position.x - box.size.x * transform.lossyScale.x / 2;
+		y = transform.position.y - box.size.y * transform.lossyScale.y / 2;
+
+		player = GameObject.Find("Player").transform;
+		y -= .5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CameraFollow camera = GameObject.Find("Main Camera").GetComponent<CameraFollow>();
+		if(player.position.x > x && player.position.y > y && player.position.x < x + w && player.position.y < y + h) {
+			camera.zSmooth = smooth;
+			Vector3 v = transform.Find("PullTo").position;
+			camera.pullTo = new Vector3(v.x, v.y, camera.zStart - size);
+			camera.pullSmooth = smooth;
+		}
+		else {
+			camera.pullTo = Vector3.zero;
+			camera.additionalZ = 0;
+		}
+	}
+}
