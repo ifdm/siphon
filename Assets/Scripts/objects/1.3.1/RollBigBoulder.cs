1eaa61d6253b684f5afb5b1dffa70c399af5eac4
Trey Cordova <treycordova@hotmail.com>
2014-03-28 22:18:51 -0600

    Extensive work on 1.3.1;

---
+++ Assets/Scripts/objects/1.3.1/RollBigBoulder.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollBigBoulder : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.gameObject.tag == "Player") {
+			GameObject.Find("Boulder").rigidbody2D.isKinematic = false;
+		}
+	}
+}
