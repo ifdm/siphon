fe00216da6d78a912df166246275d5090553feb8
tie372 <tie.372@gmail.com>
2014-04-12 14:56:47 -0600

    Add DislodgeBranch;

---
+++ Assets/Scripts/objects/1.2.2/DislodgeBranch.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DislodgeBranch : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.tag == "Player") {
+			GameObject branch = GameObject.Find("Broken Branch");
+			branch.rigidbody2D.isKinematic = false;
+			branch.transform.Find("body").GetComponent<BoxCollider2D>().isTrigger = true;
+			branch.transform.Find("leftEdge").GetComponent<BoxCollider2D>().isTrigger = true;
+			branch.transform.Find("rightEdge").GetComponent<BoxCollider2D>().isTrigger = true;
+			branch.transform.Find("bottomEdge").GetComponent<BoxCollider2D>().isTrigger = true;
+		}
+	}
+}
