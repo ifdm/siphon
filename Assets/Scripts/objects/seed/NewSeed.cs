a4f390ee17f9d211803e36273935ca80fcd8d7f5
Bjorn <tie.372@gmail.com>
2014-04-19 10:13:44 -0700

    Start NewSeed animation; Fix interacting state bug;

---
+++ Assets/Scripts/objects/seed/NewSeed.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewSeed : MonoBehaviour {
+
+	public float alpha;
+
+	void Start () {
+		transform.position = new Vector3(0, 0, 1);
+		alpha = 0;
+	}
+	
+	void Update () {
+		Color color = renderer.material.color;
+		color.a = alpha;
+		renderer.material.color = color;
+		alpha = Mathf.Lerp(alpha, 1, 5 * Time.deltaTime / 2);
+		GameObject camera = GameObject.Find("Main Camera");
+		float x = Mathf.Abs(camera.transform.position.z) * Mathf.Tan(Mathf.Deg2Rad * camera.GetComponent<Camera>().fieldOfView / 2);
+		transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x - x / 2, transform.position.y, transform.position.z), Time.deltaTime);
+	}
+}
