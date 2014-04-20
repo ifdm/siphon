df689453f90e0ae33b290279f872f9a29427222e
Trey Cordova <treycordova@hotmail.com>
2014-04-20 01:55:15 -0600

    Fixed the first pushable tree in 1.1.2;

---
+++ Assets/TankbotDeath.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankbotDeath : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.name == "Tree") {
+			var animator = transform.Find("Animation").GetComponent<TankbotAnimator>();
+			animator.One("Die");
+		}
+	}
+
+}
