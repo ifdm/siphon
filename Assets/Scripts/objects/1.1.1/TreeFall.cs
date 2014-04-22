a3eefea148e4d1c9460207b6b4cb8c4c40a1ea03
JeannetteArroyo <odowd450@158-106-iMac03.local>
2014-04-21 18:36:55 -0600

    syncing

--- Assets/Scripts/objects/1.1.1/TreeFall.cs
+++ Assets/Scripts/objects/1.1.1/TreeFall.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class TreeFall : MonoBehaviour {
 
-	public Sprite ravagedTreeSprite;
 	private bool fallen = false;
 
 	void OnTriggerEnter2D(Collider2D col) {
@@ -20,20 +19,18 @@ public class TreeFall : MonoBehaviour {
 		GameObject comet = GameObject.Find("Comet");
 		GameObject tree = GameObject.Find("FallingTree");
 		EntityAudio audio = GetComponent<EntityAudio>();
-		audio.One("Comet");
+		audio.One("Comet", 1, false, -25);
 		yield return new WaitForSeconds(2.5f);
 		camera.GetComponent<CameraFollow>().shake = 2.3f;
 		camera.GetComponent<CameraFollow>().shakeStrength = 1.0f;
 		yield return new WaitForSeconds(2.8f);
 		comet.AddComponent("Comet");
-		yield return new WaitForSeconds(0.4f);
+		yield return new WaitForSeconds(0.5f);
 		player.GetComponent<PlayerPhysics>().disableControl = false;
 		tree.rigidbody2D.isKinematic = false;
 		tree.rigidbody2D.AddTorque(-9000000);
 		audio.One("Tree_Fall_Heavy");
 //tree.rigidbody2D.AddForceAtPosition(new Vector2(1000, 0), new Vector2(0, -50));
 		tree.AddComponent("TouchOfDeath");
-		tree.transform.Find("Renderer").GetComponent<SpriteRenderer>().sprite = ravagedTreeSprite;
-
 	}
 }
