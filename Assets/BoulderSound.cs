b4ec639cb5120e502ee9042ace867ffa46bef9cd
JeannetteArroyo <odowd450@158-106-imac03.unm.edu>
2014-04-25 17:14:31 -0600

    Boulder sound in 1.3.1

--- Assets/BoulderSound.cs
+++ Assets/BoulderSound.cs
@@ -6,7 +6,7 @@ public class BoulderSound : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (rigidbody2D.velocity.x > 0.01) {
-			GetComponent<EntityAudio>().Play("roll", 1f, true);
+			GetComponent<EntityAudio>().One("roll", 1f, true);
 		}
 	}
 
