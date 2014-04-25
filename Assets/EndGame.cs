2c2c8fd3692541ccecba5c4d30e69e59ddab52f6
Evan <odowd450@158-106-imac04.unm.edu>
2014-04-25 17:34:52 -0600

    made some credit preparations 1.3.4

---
+++ Assets/EndGame.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+
+	public float zoomTo = 600;
+	public float zoomSpeed = 15;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.name == "Player") {
+			col.gameObject.rigidbody2D.isKinematic = false;
+			StartCoroutine(zoomOut());
+		}
+	}
+
+	IEnumerator zoomOut() {
+		GameObject camera = GameObject.Find ("Main Camera");
+		Destroy(camera.GetComponent<CameraFollow> ());
+
+		while (camera.transform.position.z > -zoomTo) {
+			camera.transform.position -= Vector3.forward * Time.deltaTime * zoomSpeed;
+			yield return new WaitForSeconds(0.1f);
+		}
+	}
+}
