016991a86850a7e1c7a55315f54e64447870c284
Trey Cordova <treycordova@hotmail.com>
2014-04-26 16:05:57 -0600

    Boulder Poopers die gloriously;

--- Assets/CrushBoulder.cs
+++ Assets/CrushBoulder.cs
@@ -11,11 +11,20 @@ public class CrushBoulder : MonoBehaviour {
 	}
 
 	IEnumerator Crush() {
-		while(transform.localScale.y > .4) {
+		var animation = transform.Find("Animation");
+
+		while(transform.localScale.y > .4f) {
 			transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - Time.deltaTime / 3, transform.localScale.z);
 			transform.position = new Vector3(transform.position.x, transform.position.y - (1.2f * transform.localScale.y * Time.deltaTime), transform.position.z);
-			renderer.material.color = Color.Lerp(Color.white, Color.gray, (.75f - transform.localScale.y) / .35f);
+			animation.renderer.material.color = Color.Lerp(Color.white, Color.gray, (.75f - transform.localScale.y) / .35f);
 			yield return new WaitForSeconds(0);
+		}
+
+		if(transform.localScale.y <= .4f) {
+			animation.GetComponent<Animator>().speed = 0.0f;
+			transform.Find("Steam_A").GetComponent<ParticleSystem>().Stop();
+			transform.Find("Steam_B").GetComponent<ParticleSystem>().Stop();
+			transform.Find("Steam_C").GetComponent<ParticleSystem>().Stop();
 		}
 	}
 }
