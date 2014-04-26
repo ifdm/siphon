5090e4429e72cfede069cf0921f6a446cc1a3ac2
Trey Cordova <treycordova@hotmail.com>
2014-04-25 18:24:28 -0600

    Twig now has a cracking and breaking state;

--- Assets/Scripts/lib/JumpBreak.cs
+++ Assets/Scripts/lib/JumpBreak.cs
@@ -7,9 +7,16 @@ public class JumpBreak : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D col) {
 		if(col.collider.tag == "Player" && col.relativeVelocity.y > 8) {
+			var animator = transform.Find("Animation").GetComponent<BraceAnimator>();
+
 			if(--health <= 0) {
 				GetComponent<EntityAudio>().One("Branch_Break");
-				Destroy(gameObject);
+				animator.Set("Break");
+				Destroy(gameObject.GetComponent<BoxCollider2D>());
+			}
+
+			if(health == 1) {
+				animator.Set("Crack");
 			}
 		}
 	}
---
+++ Assets/Scripts/objects/1.1.2/BraceAnimator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Spine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BraceAnimator : Animateur {
+
+	public override void Start() {
+		actions = new Dictionary<string, Animateur.AnimationType>() {
+			{"Idle", new AnimationType(Type.Spine, "idle")},
+			{"Crack", new AnimationType(Type.Spine, "cracks")},
+			{"Break", new AnimationType(Type.Spine, "breaks")}
+		};
+
+		base.Start();
+	}
+
+}
