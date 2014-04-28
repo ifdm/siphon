00d24c447187c95b5fcac93cc1d7c701805ff506
Bjorn <tie.372@gmail.com>
2014-04-28 00:39:20 -0600

    1.1 false platforms; Menu wip;

---
+++ Assets/DelayedFall.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayedFall : MonoBehaviour {
+
+	public float delay;
+	public GameObject victim;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.tag == "Player") {
+			StartCoroutine(destroy());
+		}
+	}
+
+	IEnumerator destroy() {
+		yield return new WaitForSeconds(delay);
+		if(victim) {
+			foreach(BoxCollider2D col in victim.GetComponentsInChildren<BoxCollider2D>()) {
+				col.isTrigger = true;
+			}
+			victim.rigidbody2D.isKinematic = false;
+		}
+	}
+}
