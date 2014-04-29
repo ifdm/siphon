2a3e2cfca0359faaf2d1d6d4bcdd9a777a5cc2af
Bjorn <tie.372@gmail.com>
2014-04-29 01:21:31 -0600

    Start pause menu;

---
+++ Assets/MenuPauseResume.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPauseResume : MonoBehaviour {
+
+	void MenuMouseClick() {
+		SendMessageUpwards("TogglePause", null, SendMessageOptions.DontRequireReceiver);
+	}
+}
