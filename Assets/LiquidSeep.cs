5e3378d70da8d79fa044b6b7da651d20c994cb84
Bjorn <tie.372@gmail.com>
2014-04-29 19:34:14 -0600

    LiquidSeep;

---
+++ Assets/LiquidSeep.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LiquidSeep : MonoBehaviour {
+
+	private float y;
+
+	void Start() {
+		y = transform.position.y;
+	}
+
+
+	void Update() {
+		if(transform.position.y < y + 15) {
+			transform.position += Vector3.up * Time.deltaTime * .22f;
+		}
+	}
+}
---
+++ Assets/StartLiquid.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartLiquid : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.gameObject.name == "Player") {
+			transform.Find("Liquid").gameObject.AddComponent("LiquidSeep");
+		}
+	}
+}
